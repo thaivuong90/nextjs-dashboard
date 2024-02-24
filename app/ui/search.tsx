33919c4041b9348dff117fb286ef4b48000f176a
vuonglt <thaivuong1503@gmail.com>
2024-02-24 22:58:04 +0700

    Fix type 1

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -10,6 +10,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
   const handleSearch = useDebouncedCallback((term: string) => {
     console.log(`Searching... ${term}`);
+    if (!searchParams) return '';
     const params = new URLSearchParams(searchParams);
     params.set('page', '1');
     if (term) {
@@ -30,7 +31,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
-        defaultValue={searchParams.get('query')?.toString()}
+        defaultValue={searchParams?.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
