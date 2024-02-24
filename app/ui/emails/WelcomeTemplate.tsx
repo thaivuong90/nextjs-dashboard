dbe6cb7b740e8ef35c4b3d06703834bc7f96e8ef
vuonglt <thaivuong1503@gmail.com>
2024-02-24 22:51:11 +0700

    Add send mail

---
+++ app/ui/emails/WelcomeTemplate.tsx
@@ -0,0 +1,50 @@
+import { Html } from "@react-email/html";
+import { Text } from "@react-email/text";
+import { Section } from "@react-email/section";
+import { Container } from "@react-email/container";
+
+type EmailProps = {
+  email: string;
+  password: string;
+}
+
+export default function WelcomeEmail({
+  email,
+  password
+}: EmailProps) {
+  return (
+    <Html>
+      <Section style={main}>
+        <Container style={container}>
+          <Text style={heading}>Hi there!</Text>
+          <Text style={paragraph}>Email: {email}</Text>
+          <Text style={paragraph}>Password: {password}</Text>
+        </Container>
+      </Section>
+    </Html>
+  );
+}
+
+// Styles for the email template
+const main = {
+  backgroundColor: "#ffffff",
+};
+
+const container = {
+  margin: "0 auto",
+  padding: "20px 0 48px",
+  width: "580px",
+};
+
+const heading = {
+  fontSize: "32px",
+  lineHeight: "1.3",
+  fontWeight: "700",
+  color: "#484848",
+};
+
+const paragraph = {
+  fontSize: "18px",
+  lineHeight: "1.4",
+  color: "#484848",
+};
---
+++ pages/api/sendmail.ts
@@ -0,0 +1,20 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { render } from '@react-email/render';
+import WelcomeTemplate from '../../app/ui/emails/WelcomeTemplate';
+import { sendEmail } from '../../app/lib/email';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method === 'POST') {
+    await sendEmail({
+      to: req.body.to,
+      subject: req.body.subject,
+      html: render(WelcomeTemplate({ email: req.body.to, password: 'abc123'})),
+    });
+
+    return res.status(200).json({ message: 'Email sent successfully' });
+  }
+  
+}
