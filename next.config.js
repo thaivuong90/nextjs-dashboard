2391633635f9bb0378c5aafe7a5faddad42e14de
vuonglt <thaivuong1503@gmail.com>
2024-02-25 16:17:32 +0700

    Update cors

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   async headers() {
     return [
       {
@@ -22,5 +21,3 @@ const nextConfig = {
     ];
   },
 };
-
-module.exports = nextConfig;
